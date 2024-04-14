f2395a3c5dd7f82f5cbeeb4db68533cb7946955b
Kavya Gaur <Kavyagaur36@gmail.com>
2024-04-14 15:29:11 +0530

    [UPDATE]: Listings, blogs title in url

--- src/data/types.ts
+++ src/data/types.ts
@@ -72,6 +72,7 @@ export interface PostDataType {
   date: Date;
   thumbnailImage: string;
   _id: any;
+  slug?: any;
 }
 
 export type TwMainColor =
@@ -116,6 +117,7 @@ export interface StayDataType {
   address?: any;
   imageUrls?: any;
   _id?: any;
+  slug?: any;
 }
 
 //
